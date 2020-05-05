01604a85de97fd919428068a4967ac8008e89a26
Anjali Sharma <anjalisharma060400@gmail.com>
2020-05-05 21:05:46 +0530

    Update AdminService.java

--- videohoster-service/src/main/java/com/upgrad/videohoster/service/business/AdminService.java
+++ videohoster-service/src/main/java/com/upgrad/videohoster/service/business/AdminService.java
@@ -21,12 +21,44 @@ public class AdminService {
     public VideoEntity getVideo(final String videoUuid, final String authorization) throws VideoNotFoundException, UnauthorizedException, UserNotSignedInException {
 
         UserAuthTokenEntity userAuthTokenEntity = videoDao.getUserAuthToken(authorization);
+        VideoEntity videoEntity = videoDao.getVideo(videoUuid);
+        if (videoEntity == null) {
+            throw new VideoNotFoundException("VID-001", "Video with Uuid not found");
+        }
+        String role = userAuthTokenEntity.getUser().getRole();
+        if (role.equals("admin")) {
+//            VideoEntity videoEntity = videoDao.getVideo(videoUuid);
+            if (videoEntity == null) {
+                throw new VideoNotFoundException("VID-001", "Video with Uuid not found");
+            }
+            return videoEntity;
+        } else
+            throw new UnauthorizedException("ATH-001", "UNAUTHORIZED Access, Entered user is not an admin");
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
     public VideoEntity updateVideo(final VideoEntity videoEntity, final String authorization) throws VideoNotFoundException, UnauthorizedException, UserNotSignedInException {
         UserAuthTokenEntity userAuthTokenEntity = videoDao.getUserAuthToken(authorization);
-
+        if (userAuthTokenEntity == null) {
+            throw new UserNotSignedInException("USR-001", "You are not Signed in, sign in first to update the details of the video");
+        }
         String role = userAuthTokenEntity.getUser().getRole();
+         if (role.equals("admin")) {
+
+            VideoEntity existingvideoEntity = videoDao.getVideoById(videoEntity.getId());
+
+            if (existingvideoEntity == null) {
+                throw new VideoNotFoundException("VID-002", "Video with given id is not found");
+            }
+            videoEntity.setUuid(existingvideoEntity.getUuid());
+            videoEntity.setNo_of_likes(existingvideoEntity.getNo_of_likes());
+            videoEntity.setUser_id(existingvideoEntity.getUser_id());
+            videoEntity.setCreated_at(existingvideoEntity.getCreated_at());
+
+            return videoDao.updateVideo(videoEntity);
+
+        } else {
+           throw new UnauthorizedException("ATH-001","Unauthorized Access, Entered User is not an admin");
+        }
     }
 }
