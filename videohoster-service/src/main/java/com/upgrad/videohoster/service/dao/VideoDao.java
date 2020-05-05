5e3b99341936559687fe4c276cc39581067d2007
Piyush Nautiyal <piyush.nautiyal85@gmail.com>
2020-05-05 21:09:49 +0530

    Update VideoDao.java

--- videohoster-service/src/main/java/com/upgrad/videohoster/service/dao/VideoDao.java
+++ videohoster-service/src/main/java/com/upgrad/videohoster/service/dao/VideoDao.java
@@ -16,15 +16,34 @@ public class VideoDao {
 
     public VideoEntity createVideo(VideoEntity videoEntity) {
         entityManager.persist(videoEntity);
+        return videoEntity;
     }
 
     public UserAuthTokenEntity getUserAuthToken(final String accesstoken) {
+        try {
+            return entityManager.createNamedQuery("userAuthTokenByAccessToken", UserAuthTokenEntity.class).setParameter("accessToken", accesstoken).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 
     public VideoEntity getVideo(final String videoUuid) {
+        try {
+            return entityManager.createNamedQuery("VideoEntityByUuid", VideoEntity.class).setParameter("uuid", videoUuid).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 
     public VideoEntity getVideoById(final long Id) {
+        try
+        {
+            return entityManager.createNamedQuery("VideoEntityById",VideoEntity.class).setParameter("id",Id).getSingleResult();
+        }
+        catch (NoResultException nre)
+        {
+            return null;
+        }
     }
 
     public VideoEntity updateVideo(final VideoEntity videoEntity) {
